fed1c246059986550690391c2ecab035204099c7
ornornro <roornorn@gmail.com>
2022-11-08 22:52:35 +0900

    20221108

--- src/main/java/j19_컬렉션/StudentSet.java
+++ src/main/java/j19_컬렉션/StudentSet.java
@@ -1,10 +1,12 @@
 package j19_컬렉션;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StudentSet {
 
     public static void main(String[] args) {
-
-        Set<student> students = new HashSet<student>();
+        Set<Student> students = new HashSet<Student>();
         students.add(new Student(20220001, "김준일"));
         students.add(new Student(20220002, "정빈"));
         students.add(new Student(20220003, "김규민"));
@@ -12,15 +14,16 @@ public class StudentSet {
         students.add(new Student(20220005, "김혜진"));
         students.add(new Student(20220006, "김지향"));
 
+        System.out.println(students);
+
         /*
             1. 김혜진 학생의 학번을 출력하세요.
             2. 20220004 학번을 가진 학생의 이름을 설민수로 변경하세요.
             3. 20220001 학번이 존재하면 해당 학생을 삭제하세요.
 
             set -> list 변환
-            해당 list 를 학번 순서에 맞게 정렬하시오.
+            해당 list를 학번 순서에 맞게 정렬하시오.
          */
-
     }
 
 }
