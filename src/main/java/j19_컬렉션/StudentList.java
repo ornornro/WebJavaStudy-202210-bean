20a927e2c3013fefecbd137df7c995b86b128839
ornornro <roornorn@gmail.com>
2022-11-08 20:20:00 +0900

    점검

--- src/main/java/j19_컬렉션/StudentList.java
+++ src/main/java/j19_컬렉션/StudentList.java
@@ -1,6 +1,8 @@
 package j19_컬렉션;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class StudentList {
@@ -50,6 +52,62 @@ public class StudentList {
 
         System.out.println(students);
 
+        /*
+            id = 20220003학생을 찾아서 그 학생의 이름이 김규민이면 김경민으로 바꿔라
+         */
 
+        searchId = 20220003;
+
+        for(Student student : students) {
+            if(student.getId() == searchId && student.getName().equals("김규민")) {
+                student.setName("김경민");
+                break;
+            }
+        }
+
+        System.out.println(students);
+
+        System.out.println("-------------------------------");
+
+        for(Student student : students) {
+            System.out.println(student);
+        }
+
+        System.out.println();
+
+        searchId = 20220004;
+
+        /*
+            박경호를 박창우로 변경
+         */
+
+        Iterator<Student> iterator = students.iterator();
+        while(iterator.hasNext()) {
+            Student student = iterator.next();
+            if(student.getId() == searchId) {
+                student.setName("박창우");
+                break;
+            }
+        }
+
+        System.out.println(students);
+
+        List<Student> reverseStudents = new ArrayList<Student>();
+
+//        for(int i = 0; i < students.size(); i++) {
+//            reverseStudents.add(students.get(students.size() - 1 - i));
+//        }
+//
+//        System.out.println(reverseStudents);
+
+        for(Student student : students) {
+            reverseStudents.add(0, student);
+        }
+
+        System.out.println(reverseStudents);
+
+        Collections.reverse(reverseStudents);
+
+        System.out.println(reverseStudents);
     }
 }
